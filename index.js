dae45fe7e7f1ac29beeede85fc46ad32344c391f
Connor <ConnorKirkpatrick3@gmail.com>
2021-05-07 21:58:27 +0100

    added button, body and html attributes to the style sheet

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const path = require("path");
 const app = require("express")();
+const express = require("express")
 const http = require("http").createServer(app);
 
-app.use("/static", app.static(path.join(__dirname, "./static/")));
+app.use("/static", express.static(path.join(__dirname, "./static/")));
 
 let PORT = Number(process.env.PORT || 80);
 http.listen(PORT, () => {
     console.log("Listening on " + PORT);
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/static/lightsPage.html");
 });
