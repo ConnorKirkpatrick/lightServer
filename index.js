f960a877e5d2577bf541a20ab3846cf17adc923b
Connor <ConnorKirkpatrick3@gmail.com>
2021-05-07 21:45:19 +0100

    started to update the old webpage to new standards. added empty style sheet

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const path = require("path");
 const app = require("express")();
 const http = require("http").createServer(app);
 
-app.use("/static", express.static(path.join(__dirname, "./static/")));
+app.use("/static", app.static(path.join(__dirname, "./static/")));
 
 let PORT = Number(process.env.PORT || 80);
 http.listen(PORT, () => {
