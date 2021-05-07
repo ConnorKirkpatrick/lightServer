306947da0bb17e99523ec8b8070cf014d9259052
Connor <ConnorKirkpatrick3@gmail.com>
2021-05-07 21:37:27 +0100

    updated git.ignore to ignore node files added the basic server listener and static directory support to the index

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = require("express")();
+const http = require("http").createServer(app);
+
+app.use("/static", express.static(path.join(__dirname, "./static/")));
+
+let PORT = Number(process.env.PORT || 80);
+http.listen(PORT, () => {
+    console.log("Listening on " + PORT);
+});
