d616f0dbcefdb0fc7a1dba7f227bfb00012eaa6f
Connor <ConnorKirkpatrick3@gmail.com>
2021-08-02 15:35:21 +0100

    Updated webpage and on/off/auto functionality

--- arduino/lightSensorServer/lightSensorServer.ino
+++ arduino/lightSensorServer/lightSensorServer.ino
@@ -5,12 +5,14 @@
 
 
 //data for connection to the net for time
-char* ssid = "SKYSAA2U";
-char* password = "Wwy7QKf37hCN";
+//char* ssid = "SKYSAA2U";
+//char* password = "Wwy7QKf37hCN";
+const char* ssid = "Lights";
+const char* password = "jjscd123";
 
-IPAddress local_IP(192,168,1,87);
-IPAddress gateway(192,168,4,9);
-IPAddress subnet(255,255,255,0);
+IPAddress local_IP(192, 168, 1, 87);
+IPAddress gateway(192, 168, 4, 9);
+IPAddress subnet(255, 255, 255, 0);
 
 //data for the operation of the webpage
 String header;
@@ -25,12 +27,6 @@ AsyncWebServer aServer(80);
 const int ldrPin = A0;
 const int OutputPin = D5;
 const int LedPin = D0;
-bool SensorStatus = false;
-bool webOverride = false;
-bool Status = false;
-int OnTime = 15;
-int OffTime = 24;
-int setLightLevel = 300;
 int currentLightLevel = 0;
 
 
@@ -40,26 +36,23 @@ void setup() {
   Serial.begin(9600);
 
   WiFi.mode(WIFI_STA);
-  
-  pinMode(LedPin,OUTPUT);
+
+  pinMode(LedPin, OUTPUT);
   digitalWrite(LedPin, HIGH);
-  
+
   pinMode(OutputPin, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
-  
+
   pinMode(ldrPin, INPUT);
 
-   
+
   SetupNet();
   server.begin();
 
-  Serial.print("SSID: "); Serial.println(WiFi.SSID());
-  Serial.print("Signal: "); Serial.println(WiFi.RSSI());
-  
 
-  aServer.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    String message = "Hi! I am the LightsServer.\nWifi: "+WiFi.SSID()+"\nIP: "+WiFi.localIP().toString()+"\nSignal Interference(less is better): "+WiFi.RSSI()+"\n\nhttp://"+WiFi.localIP().toString()+"/update";
+  aServer.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+    String message = "Hi! I am the LightsServer.\nWifi: " + WiFi.SSID() + "\nIP: " + WiFi.localIP().toString() + "\nSignal Interference(less is better): " + WiFi.RSSI() + "\n\nhttp://" + WiFi.localIP().toString() + "/update";
     request->send(200, "text/plain", message);
   });
   AsyncElegantOTA.begin(&aServer);    // Start ElegantOTA
@@ -70,37 +63,37 @@ void setup() {
 
 
 void loop() {
-    AsyncElegantOTA.loop();
-    WiFiClient client = server.available(); 
-    if (client) {                             
-        Serial.println("New Client.");
-        while(client){
-          String message = client.readStringUntil('\n');    // receives the message from the client
-          if(message == "ON"){
-            Serial.println("TURNING ON");
-            digitalWrite(LED_BUILTIN, LOW);
-          }
-          
-          if(message == "OFF"){
-            Serial.println("TURNING OFF");
-            digitalWrite(LED_BUILTIN, HIGH);
-          }
-          
-          if(message == "LEVEL"){
-            Serial.println("GETTING LEVEL");
-            client.print("123");
-            client.flush();
-          }
-        }
-        Serial.println("Client Disconnected");
+  AsyncElegantOTA.loop();
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("New Client.");
+    while (client) {
+      String message = client.readStringUntil('\n');    // receives the message from the client
+      if (message == "ON") {
+        Serial.println("TURNING ON");
+        digitalWrite(OutputPin, HIGH);
+      }
+
+      if (message == "OFF") {
+        Serial.println("TURNING OFF");
+        digitalWrite(OutputPin, LOW);
+      }
+
+      if (message == "LEVEL") {
+        Serial.println("GETTING LEVEL");
+        client.print(getStrLevel());
+        client.flush();
+      }
     }
+    Serial.println("Client Disconnected");
+  }
 }
 
-void SetupNet(){
+void SetupNet() {
   //seting up private net for use
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.println("Attempting to connect to: "+String(ssid));
+    Serial.println("Attempting to connect to: " + String(ssid));
     delay(2000);
   }
   Serial.println("\nConnected");
@@ -113,11 +106,9 @@ void SetupNet(){
 }
 
 
-int getLevel(){
+int getLevel() {
   return analogRead(ldrPin);
 }
-String getStrLevel(){
+String getStrLevel() {
   return (String)getLevel();
 }
-
- 
